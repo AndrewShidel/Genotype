fac0db4d5058543d591583e8866b9bed5c004a6a
AndrewShidel <andrew@shidel.com>
2015-06-24 16:48:45 -0400

    test.asm is now complient with new interpreter

--- test.asm
+++ test.asm
@@ -1,31 +1,32 @@
 JMP 18; Check if index is in array
-LDI 4;  ac <- array[i]
-SUB 1;  AC <- AC - max_ele
+LDI 3;  ac <- array[i]
+SUB 0;  AC <- AC - max_ele
 JMN 11; AC < 0
-LDI 4;  load index into ac
-STA 1;  store ac in the MAX var
-LDA 4;  ac <- array_index
-ADD 3;  ac++
-STA 4;  store current index
+LDI 3;  load index into ac
+STA 0;  store ac in the MAX var
+LDA 3;  ac <- array_index
+ADD 2;  ac++
+STA 3;  store current index
 JMP 1;  process next item
-LDA 4;  load index                  --- Begin increment index
-ADD 3;  pc++
-STA 4;  store index
+LDA 3;  load index                  --- Begin increment index
+ADD 2;  pc++
+STA 3;  store index
 JMP 1;  process next item
-LDA 1;  load largest found          ---- Begin Halt
-STI 2;  store ac as return val
+LDA 0;  load largest found          ---- Begin Halt
+STI 1;  store ac as return val
 HLT ;
-LDA 4;  load current index          ---- Begin bound check
-SUB 2;  ac <- ac - return_index
+LDA 3;  load current index          ---- Begin bound check
+SUB 1;  ac <- ac - return_index
 JMN 2;  jump to the beginning if negative
 JMP 15; halt if >= 0
+HLT ;
 &
-1   0 ; Max Size found
-2   9; index of return
-3   1 ; Constant 1 used for incrimenting
-4   5 ; array start
-5   5 ; array[1]
-6   9 ; array[2]
-7   35; array[3]
-8   8 ; array[4]
-9   0 ; location for result
+0 ; Max Size found
+8; index of return
+1 ; Constant 1 used for incrimenting
+4 ; array start
+5 ; array[1]
+7 ; array[2]
+3; array[3]
+8 ; array[4]
+0 ; location for result
