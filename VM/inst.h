91d9cec07b99869ae863d1486d98acde124b38d6
AndrewShidel <andrew@shidel.com>
2015-09-12 14:35:21 -0400

    Adds NOP code, OP_COUNT const, and Instruciton constructors

--- VM/inst.h
+++ VM/inst.h
@@ -3,9 +3,18 @@
 #ifndef INSTHEADER
 #define INSTHEADER
 
-enum OPCODES { LDA, LDI, STA, STI, ADD, SUB, JMP, JMZ, JMN, JAL, ALC, DLC, SYS, HLT };
+const int OP_COUNT = 15;
+enum OPCODES { LDA, LDI, STA, STI, ADD, SUB, JMP, JMZ, JMN, JAL, ALC, DLC, SYS, HLT, NOP };
 
 struct Instruction {
+	Instruction(OPCODES _opcode, int _operand) {
+		opcode = _opcode;
+		operand = _operand;
+	}
+	Instruction() {
+		opcode = NOP;
+		operand = 0;
+	}
 	OPCODES opcode;
 	int operand;
 };
